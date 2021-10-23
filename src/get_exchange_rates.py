621c3c982cd1bde87234cd69ceb71ebf8d87601f
Leonard Schurbert <ls@hopkins.systems>
2021-10-23 22:49:49 +0200

    refactored model and load

--- src/get_exchange_rates.py
+++ src/get_exchange_rates.py
@@ -1,4 +1,4 @@
-import os
+import json
 from datetime import date, timedelta
 import concurrent.futures
 import urllib.request
@@ -7,80 +7,116 @@ from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from .model import Rate, Base_currency
+from model import create_schema, Exchange_rate, Target_currency, Base_currency
 
 logging.basicConfig(filename='get_exchange_rates.log',
                     format='%(asctime)s %(levelname)s:%(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    encoding='utf-8',
+                    datefmt='%Y-%m-%d %I:%M:%S %p',
                     level=logging.DEBUG)
 
+SUPPORTED_BASE_SYMBOLS = ["EUR", "USD"]
 BASE_URL = 'http://api.exchangeratesapi.io/v1'
-DATES_TO_RETRIEVE = 250
-
-API_ACCESS_KEY = ''
-# API_ACCESS_KEY = config('API_ACCESS_KEY')
-# POSTGRESQL_USER = config('POSTGRESQL_USER')
-# POSTGRESQL_PW = config('POSTGRESQL_PW')
+API_ACCESS_KEY = config('API_ACCESS_KEY')
 
 
-def date_string_from_delta(delta: int):
+def get_date_stack():
     today = date.today()
-    def get_date_string():
-        date = today - timedelta(days=delta)
-        return date.strftime("%Y-%m-%d")
+    year_delta = today - date(today.year, 1, 1)
 
-    return get_date_string()
+    def date_string_from_delta(delta: int):
+        new_date = today - timedelta(days=delta)
+        return new_date.strftime("%Y-%m-%d")
 
-
-DATE_STRINGS = [
-    date_string_from_delta(delta) for delta in range(DATES_TO_RETRIEVE)
-]
-
-urls = {
-    "date": lambda date_string: f'{BASE_URL}/{date_string}?access_key={API_ACCESS_KEY}',
-    "symbols": lambda _: f'{BASE_URL}/symbols?access_key={API_ACCESS_KEY}'
-}
+    return [
+        date_string_from_delta(delta) for delta in range(year_delta.days + 1)
+    ]
 
 
-def load_url(url: str, timeout):
+def get_url(**kwargs: dict) -> str:
+    endpoint = kwargs.get("endpoint")
+    date_string = kwargs.get("date_string")
+    if endpoint == "rates":
+        return f'{BASE_URL}/{date_string}?access_key={API_ACCESS_KEY}'
+    if endpoint == "symbols":
+        return f'{BASE_URL}/symbols?access_key={API_ACCESS_KEY}'
+
+
+def load_url(url: str, timeout: int) -> dict:
     with urllib.request.urlopen(url, timeout=timeout) as conn:
-        return conn.read().decode('utf-8')
+        response_string = conn.read().decode('utf-8')
+        return json.loads(response_string)
 
 
-def load_date(date: str, timeout):
-    url = urls.get('date')(date)
-    return load_url(url, timeout)
+def load_rates_of_date(date: str, timeout: int) -> dict:
+    """ Loads all EUR-base rates per date.
+        Derives rates for all bases in BASE_TO_BE_DERIVED.
+        Returns dict with base_currency as key and target_currency: rate dicts as value"""
+
+    url = get_url(endpoint="rates", date_string=date)
+    rate_response = load_url(url, timeout)
+    rates_for_EUR = rate_response.get("rates", {})
+    rates_of_date = {"EUR": rates_for_EUR}
+    for base_symbol in (symbol for symbol in SUPPORTED_BASE_SYMBOLS
+                        if symbol != 'EUR'):
+        rates_of_date = {
+            **rates_of_date, base_symbol: {
+                target_symbol: rate * rates_for_EUR.get(base_symbol)
+                for target_symbol, rate in rate_response.get("rates",
+                                                             {}).items()
+            }
+        }
+    return rates_of_date
 
 
 def load_symbols(timeout):
-    url = urls.get('symbols')()
-    return load_url(url, timeout)
+    url = get_url(endpoint="symbols")
+    return load_url(url, timeout).get("symbols")
 
 
-def execute_load(session, date, timeout):
+def execute_load(**kwargs):
+    session = kwargs.get("session")
+    date = kwargs.get("date")
+    timeout = kwargs.get("timeout")
+    base_currencies = kwargs.get("base_currencies")
+    target_currencies = kwargs.get("target_currencies")
+
     def load():
-        logging.info(f'{date} started')
-        response = load_date(date, timeout)
-
-        # session
+        try:
+            logging.info(f'execute_load() {date} started')
+            date_rates: dict = load_rates_of_date(date, timeout)
+            for base_currency, rates in date_rates.items():
+                for target_currency, rate in rates.items():
+                    exchange_rate = Exchange_rate(
+                        base_currency_id=base_currencies.get(base_currency).id,
+                        target_currency_id=target_currencies.get(
+                            target_currency).id,
+                        date=date,
+                        rate=rate)
+                    session.add(exchange_rate)
+            session.commit()
+        except Exception as e:
+            logging.error(f'load() for {date}: {e}')
 
     return load()
 
 
-def load_data_threaded(Session, timeout, max_workers):
-    # high-level interface for asynchronously executing callables
+def load_data_threaded(**kwargs):
+    Session = kwargs.get("Session")
+    max_workers = kwargs.get("max_workers")
+    dates = kwargs.get("dates")
+
     with concurrent.futures.ThreadPoolExecutor(
             max_workers=max_workers) as executor:
         future_to_load = {}
-        for date in DATE_STRINGS:
+        for date in dates:
             session = Session()
             future_to_load = {
                 **future_to_load,
-                executor.submit(execute_load(session, date, timeout), date, 60):
+                executor.submit(
+                    execute_load(**kwargs, date=date, session=session)):
                 date
             }
-        # future_to_load = {executor.submit(execute_load(session, date, timeout), date, 60): date for date in DATE_STRINGS}
+
         for future in concurrent.futures.as_completed(future_to_load):
             current_date = future_to_load[future]
             try:
@@ -91,20 +127,50 @@ def load_data_threaded(Session, timeout, max_workers):
                 logging.info(f'{current_date} success')
 
 
+def commit_currency_relations(session, timeout):
+    try:
+        symbols = load_symbols(timeout)
+        target_currencies = {}
+        base_currencies = {}
+        for symbol, name in symbols.items():
+            target_currency = Target_currency(symbol=symbol, name=name)
+            target_currencies = {**target_currencies, symbol: target_currency}
+            session.add(target_currency)
+            if symbol in SUPPORTED_BASE_SYMBOLS:
+                base_currency = Base_currency(symbol=symbol, name=name)
+                base_currencies = {**base_currencies, symbol: base_currency}
+                session.add(base_currency)
+        session.commit()
+        return (base_currencies, target_currencies)
+    except Exception as e:
+        logging.error(f'commit_currency_relations() {repr(e)}')
+
+
 def orchestrate_data_load(timeout):
-    # allows instantiation of 1 connection per thread
-    engine = create_engine('postgresql://me@localhost/mydb',
-                           pool_size=1,
-                           max_overflow=0)
+    USER = config('POSTGRESQL_USER')
+    PASSWORD = config('POSTGRES_PASSWORD')
+    DB = config('POSTGRES_DB')
+
+    engine = create_engine(
+        f'postgresql://{USER}:{PASSWORD}@localhost/{DB}',
+        # limits engine instantiation of 1 connection per thread
+        pool_size=1,
+        max_overflow=0)
+
+    create_schema(engine)
+
     Session = scoped_session(sessionmaker(bind=engine))
     session = Session()
 
-    # instantiate SQLAlchemy classes
+    base_currencies, target_currencies = commit_currency_relations(
+        session, timeout)
+    dates = get_date_stack()
+    load_data_threaded(dates=dates,
+                       Session=Session,
+                       base_currencies=base_currencies,
+                       target_currencies=target_currencies,
+                       max_workers=25,
+                       timeout=timeout)
 
-    # get currency's symbols
-    symbols = load_symbols(timeout)
 
-    # if not exist add currency as column to table rate
-
-    # start data load
-    load_data_threaded(Session, timeout, 25)
+orchestrate_data_load(2000)
--- src/model.py
+++ src/model.py
@@ -1,43 +1,54 @@
-"""Declare models and relationships."""
-from sqlalchemy import Column, DateTime, Date, ForeignKey, Integer, String
+from sqlalchemy import Column, UniqueConstraint, Date, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
-
-from database import engine
 
 Base = declarative_base()
 
 
-class Rate(Base):
-    """ Exchange rates of currencies related to a base currency. """
-
-    __tablename__ = "rate"
+class Exchange_rate(Base):
+    """ Exchange rates of base to target currencies. """
+    __tablename__ = "exchange_rate"
 
     id = Column(Integer, primary_key=True, autoincrement="auto")
-    base_currency_id = Column(Integer, ForeignKey("base_currency.id"))
-    date = Column(Date)
-    # ... columns for each currency will be appended programtically. Naming by symbols: ["AED","AFN","ALL", ...]
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
+    base_currency_id = Column(Integer,
+                              ForeignKey("base_currency.id"),
+                              nullable=False)
+    target_currency_id = Column(Integer,
+                                ForeignKey("target_currency.id"),
+                                nullable=False)
+    rate = Column(Float, nullable=False)
+    date = Column(Date, nullable=False)
 
     base_currency = relationship("Base_currency")
+    target_currency = relationship("Target_currency")
+    __table_args__ = (UniqueConstraint('base_currency_id',
+                                       'target_currency_id',
+                                       'date',
+                                       name='rate_uc'), )
 
     def __repr__(self):
-        return "<Rate %r>" % self.id
+        return "<Exchange_rate %r>" % self.id
 
 
-class Base_currency(Base):
-    """ Base currencies, such as USD & EUR. """
-
-    __tablename__ = "base_currency"
-
+class Currency(object):
+    """ Currency symbols and names """
     id = Column(Integer, primary_key=True, index=True, autoincrement="auto")
-    symbol = Column(String(3), index=True)
+    symbol = Column(String(3), index=True, unique=True, nullable=False)
     name = Column(String)
 
     def __repr__(self):
-        return "<Base_currency %r>" % self.symbol
+        return "<Currency %r>" % self.symbol
 
 
-Base.metadata.create_all(engine)
+class Base_currency(Currency, Base):
+    """ Base currency symbols and names """
+    __tablename__ = "base_currency"
+
+
+class Target_currency(Currency, Base):
+    """ Target currency symbols and names """
+    __tablename__ = "target_currency"
+
+
+def create_schema(engine):
+    Base.metadata.create_all(engine)
